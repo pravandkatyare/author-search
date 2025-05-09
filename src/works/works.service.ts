bfd887f879ee9cb6bcdbd596e47c036d3e4527ed
pravandkatyare <36467951+pravandkatyare@users.noreply.github.com>
2025-05-09 19:32:01 +0530

    Update works service

--- src/works/works.service.ts
+++ src/works/works.service.ts
@@ -7,6 +7,17 @@ export class WorksService {
     constructor(private readonly httpService: HttpService ){}
 
     async get(key: string) {
-        return await this.httpService.axiosRef.get(WORK_URL + key +`/works.json`)
+        return await this.httpService.axiosRef
+            .get(WORK_URL + key +`/works.json`)
+            .then(response =>{
+                this.logger.debug("fetched data for auther: ", key);
+                return response.data;
+            }).catch(error => {
+                if (error.status == 404) {
+                    throw new NotFoundException("Author "+ key +" not found");
+                } else {
+                    throw new InternalServerErrorException("something went wrong");
+                }
+            });
     }
 }
